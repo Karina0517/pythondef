b6e6b36c65a0344539834a6586ca98967bfe7dc8
Karina0517 <henaokarina17@gmail.com>
2025-05-06 21:51:31 -0500

    Exercises

--- 2-exercises2/advance_functions.py
+++ 2-exercises2/advance_functions.py
@@ -2,10 +2,24 @@ import string
 import random
 
 # 1. Shopping cart simulator
+def test_calculate_cart_total(dic, iva= 0.21):
+    subT = 0
+    for i in dic:
+        priceP = i['price']
+        qnt = i['quantity']
+        subT += priceP * qnt
+    total = subT *(1 + iva)
+    return round(total,2)
 
+cart = [
+        {"name": "Shirt", "price": 20.0, "quantity": 2},
+        {"name": "Pants", "price": 40.0, "quantity": 1}
+    ]
+print(test_calculate_cart_total(cart,0.21))
 
 # 2. Text processing (word count)
 
+    
 
 # 3. Secure password generator
 
